7e8f3efeaf2be3efb2abe145aadacecc384ac97a
RuthNzikwikiza <ruthnzikwikiza7@gmail.com>
2025-04-16 09:45:45 +0200

    Initial commit: Task Manager with user auth

---
+++ tasks/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST['description']
+        Task.objects.create(title=title, description=description)
+        return redirect('task_list')
+    return render(request, 'tasks/create_task.html')
+
+def update_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.title = request.POST['title']
+        task.description = request.POST['description']
+        task.completed = 'completed' in request.POST
+        task.save()
+        return redirect('task_list')
+    return render(request, 'tasks/update_task.html', {'task': task})
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/delete_task.html', {'task': task})
